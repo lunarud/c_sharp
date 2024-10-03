e013acc99f5e82c0feb70e5dbe9648d7717fe4bf
lunarud <rudy.luna@gmail.com>
2024-10-02 23:41:57 -0500

    Update CoinCapData.cs

--- CoinCapData.cs
+++ CoinCapData.cs
@@ -1,33 +1,69 @@
 using Moq;
 using NUnit.Framework;
-using Microsoft.Extensions.Caching.Memory;
-using System.Net.Http;
-using System.Threading.Tasks;
-using Moq.Protected;
-using System.Threading;
-using System.Net;
-using System.Net.Http.Json;
-using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Memory;
 
-namespace CoinCapTests
+[TestFixture]
+public class WeatherServiceTests
 {
-    public class CoinCapData
+    private Mock<IMemoryCache> _cacheMock;
+    private Mock<HttpMessageHandler> _httpMessageHandlerMock;
+    private HttpClient _httpClient;
+    private WeatherService _weatherService;
+
+    [SetUp]
+    public void Setup()
     {
-        public string Id { get; set; }
-        public string Symbol { get; set; }
-        public string CurrencySymbol { get; set; }
-        public decimal RateUsd { get; set; }
+        _cacheMock = new Mock<IMemoryCache>();
+
+        _httpMessageHandlerMock = new Mock<HttpMessageHandler>();
+        _httpClient = new HttpClient(_httpMessageHandlerMock.Object);
+        _weatherService = new WeatherService(_cacheMock.Object, _httpClient);
     }
 
-    public class CoinCapResponse
+    [Test]
+    public async Task GetWeatherDataAsync_ShouldReturnCachedData_WhenCacheHit()
     {
-        public CoinCapData Data { get; set; }
+        // Arrange
+        var cachedData = "Sunny";
+        _cacheMock.Setup(x => x.TryGetValue(It.IsAny<object>(), out cachedData)).Returns(true);
+
+        // Act
+        var result = await _weatherService.GetWeatherDataAsync();
+
+        // Assert
+        Assert.AreEqual(cachedData, result);
+        _cacheMock.Verify(x => x.TryGetValue(It.IsAny<object>(), out cachedData), Times.Once);
+        _httpMessageHandlerMock.VerifyNoOtherCalls();
     }
 
-    [TestFixture]
-    public class CoinCapServiceTests
+    [Test]
+    public async Task GetWeatherDataAsync_ShouldCallApiAndCacheResponse_WhenCacheMiss()
     {
-        private Mock<IMemoryCache> _memoryCacheMock;
-        private Mock<IHttp
+        // Arrange
+        var cachedData = (string)null; // No cached data
+        _cacheMock.Setup(x => x.TryGetValue(It.IsAny<object>(), out cachedData)).Returns(false);
+
+        var apiResponseContent = new StringContent("Rainy");
+        var responseMessage = new HttpResponseMessage(HttpStatusCode.OK) { Content = apiResponseContent };
+
+        _httpMessageHandlerMock
+            .Setup(m => m.SendAsync(It.IsAny<HttpRequestMessage>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(responseMessage);
+
+        _cacheMock.Setup(x => x.Set(It.IsAny<object>(), It.IsAny<object>(), It.IsAny<TimeSpan>()))
+            .Returns((object key, object value, TimeSpan ts) => value);
+
+        // Act
+        var result = await _weatherService.GetWeatherDataAsync();
+
+        // Assert
+        Assert.AreEqual("Rainy", result);
+        _httpMessageHandlerMock.Verify(x => x.SendAsync(It.IsAny<HttpRequestMessage>(), It.IsAny<CancellationToken>()), Times.Once);
+        _cacheMock.Verify(x => x.Set(It.IsAny<object>(), It.IsAny<object>(), It.IsAny<TimeSpan>()), Times.Once);
+    }
+}
