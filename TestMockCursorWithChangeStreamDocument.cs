3e77a7cba7e8cd4ad56fecb076d6ab620626ef5f
lunarud <rudy.luna@gmail.com>
2025-03-20 00:35:21 -0500

    Update and rename testit.cs to TestMockCursorWithChangeStreamDocument.cs

--- TestMockCursorWithChangeStreamDocument.cs
+++ TestMockCursorWithChangeStreamDocument.cs
@@ -1,3 +1,199 @@
+using Moq;
+using NUnit.Framework;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using System.Collections.Generic;
+
+[TestFixture]
+public class ChangeStreamTests
+{
+    [Test]
+    public void TestChangeStreamCursorWithFullDocument()
+    {
+        // Arrange
+        // Create a sample TestEntity
+        var testEntity = new TestEntity
+        {
+            Id = ObjectId.GenerateNewId(),
+            Name = "TestName",
+            Value = 42
+        };
+
+        // Create a sample ChangeStreamDocument
+        var changeStreamDoc = new ChangeStreamDocument<TestEntity>(
+            operationType: ChangeStreamOperationType.Insert,
+            fullDocument: testEntity,
+            ns: new ChangeStreamDocument<TestEntity>.NamespaceInfo("testDb", "testCollection"),
+            documentKey: new BsonDocument("_id", testEntity.Id),
+            updateDescription: null,
+            clusterTime: new BsonTimestamp(1, 1),
+            txnNumber: null,
+            lsid: null
+        );
+
+        // Create the mock cursor
+        var cursorMock = new Mock<IChangeStreamCursor<ChangeStreamDocument<TestEntity>>>();
+        
+        // Setup the mock to return our changeStreamDoc when Current is accessed
+        cursorMock.Setup(x => x.Current)
+            .Returns(new List<ChangeStreamDocument<TestEntity>> { changeStreamDoc });
+        
+        // Setup MoveNext to return true once then false
+        cursorMock.SetupSequence(x => x.MoveNext(default))
+            .Returns(true)
+            .Returns(false);
+
+        // Act
+        var cursor = cursorMock.Object;
+        bool hasNext = cursor.MoveNext(default);
+        var result = cursor.Current;
+
+        // Assert
+        Assert.IsTrue(hasNext);
+        Assert.IsNotNull(result);
+        Assert.AreEqual(1, result.Count());
+        
+        var retrievedDoc = result.First();
+        Assert.AreEqual(ChangeStreamOperationType.Insert, retrievedDoc.OperationType);
+        Assert.IsNotNull(retrievedDoc.FullDocument);
+        Assert.AreEqual(testEntity.Id, retrievedDoc.FullDocument.Id);
+        Assert.AreEqual(testEntity.Name, retrievedDoc.FullDocument.Name);
+        Assert.AreEqual(testEntity.Value, retrievedDoc.FullDocument.Value);
+    }
+}
+
+// Sample entity class for testing
+public class TestEntity
+{
+    public ObjectId Id { get; set; }
+    public string Name { get; set; }
+    public int Value { get; set; }
+}
+
+using Moq;
+using NUnit.Framework;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using System.Collections.Generic;
+
+[TestFixture]
+public class ChangeStreamTests
+{
+    [Test]
+    public void TestMockCursorWithChangeStreamDocument()
+    {
+        // Arrange
+        // Sample entity
+        var testEntity = new TestEntity
+        {
+            Id = ObjectId.GenerateNewId(),
+            Name = "TestName",
+            Value = 42
+        };
+
+        // Create required BsonDocuments for ChangeStreamDocument
+        var resumeToken = new BsonDocument("resumeToken", "testToken");
+        var nsDoc = new BsonDocument
+        {
+            { "db", "testDb" },
+            { "coll", "testCollection" }
+        };
+        var docKey = new BsonDocument("_id", testEntity.Id);
+        var fullDoc = testEntity.ToBsonDocument();
+
+        // Create the ChangeStreamDocument
+        var changeStreamDoc = new ChangeStreamDocument<TestEntity>(
+            resumeToken,
+            nsDoc,
+            ChangeStreamOperationType.Update,
+            docKey,
+            fullDoc,
+            null,           // updateDescription
+            new BsonTimestamp(1, 1),
+            null,           // extraElements
+            null,           // clusterTimeDoc
+            null,           // txnNumber
+            null            // lsid
+        );
+
+        // Setup Mock cursor
+        var cursorMock = new Mock<IChangeStreamCursor<ChangeStreamDocument<TestEntity>>>();
+        
+        // Configure the mock to return our changeStreamDoc
+        cursorMock.Setup(m => m.Current)
+            .Returns(new List<ChangeStreamDocument<TestEntity>> { changeStreamDoc });
+        
+        // Configure MoveNext behavior
+        cursorMock.SetupSequence(m => m.MoveNext(default))
+            .Returns(true)    // First call returns true (has data)
+            .Returns(false);  // Second call returns false (no more data)
+
+        // Act
+        var cursor = cursorMock.Object;
+        bool hasNext = cursor.MoveNext(default);
+        var results = cursor.Current;
+
+        // Assert
+        Assert.IsTrue(hasNext, "Cursor should have data");
+        Assert.IsNotNull(results, "Results should not be null");
+        Assert.AreEqual(1, results.Count(), "Should return one document");
+
+        var resultDoc = results.First();
+        Assert.AreEqual(ChangeStreamOperationType.Update, resultDoc.OperationType, "Operation type should match");
+        Assert.IsNotNull(resultDoc.FullDocument, "FullDocument should not be null");
+        Assert.AreEqual(testEntity.Id, resultDoc.FullDocument.Id, "ID should match");
+        Assert.AreEqual(testEntity.Name, resultDoc.FullDocument.Name, "Name should match");
+        Assert.AreEqual(testEntity.Value, resultDoc.FullDocument.Value, "Value should match");
+    }
+}
+
+// Sample entity class
+public class TestEntity
+{
+    public ObjectId Id { get; set; }
+    public string Name { get; set; }
+    public int Value { get; set; }
+}
+
+
+
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Moq;
+using System.Collections.Generic;
+public class ChangeStreamTest
+{
+    public void SetupMockCursor()
+    {
+        // Create a mock cursor
+        var mockCursor = new Mock<IAsyncCursor<ChangeStreamDocument<BsonDocument>>>();
+        // Create a sample ChangeStreamDocument
+        var changeStreamDocument = new ChangeStreamDocument<BsonDocument>(new BsonDocument
+        {
+            { "_id", new BsonDocument { { "_data", "825E2FC2760000000B2B022C0100296E5A100496C525567BB74BD28BFD504F987082C046645F696400645E27BCC9F94B5117D894CBC30004" } } },
+            { "operationType", "update" },
+            { "ns", new BsonDocument { { "db", "sample_training" }, { "coll", "grades" } } },
+            { "documentKey", new BsonDocument { { "_id", new ObjectId("5e27bcc9f94b5117d894cbc3") } } },
+            { "updateDescription", new BsonDocument { { "updatedFields", new BsonDocument { { "comments.14", "You will learn a lot if you read the MongoDB blog!" } } }, { "removedFields", new BsonArray() } } }
+        });
+        // Set up the mock cursor to return the sample ChangeStreamDocument
+        mockCursor.SetupSequence(c => c.MoveNext(It.IsAny<System.Threading.CancellationToken>()))
+                  .Returns(true)
+                  .Returns(false);
+        mockCursor.SetupGet(c => c.Current).Returns(new List<ChangeStreamDocument<BsonDocument>> { changeStreamDocument });
+        // Use the mock cursor in your test
+        var cursor = mockCursor.Object;
+        while (cursor.MoveNext())
+        {
+            foreach (var doc in cursor.Current)
+            {
+                // Process the ChangeStreamDocument
+                System.Console.WriteLine(doc);
+            }
+        }
+    }
+}
+
 using NUnit.Framework;
 using MongoDB.Bson;
 using MongoDB.Driver;
